aae55f60160c491af298948a81f8b2a048ac4345
William Martin <awmartin@anomalus.com>
2024-02-17 19:38:29 -0500

    Just some comments in the Simulation class.

--- src/simulation.js
+++ src/simulation.js
@@ -1,6 +1,30 @@
+/**
+ * @module Spatiality.Simulation
+ * @description Base class for Simulations.
+ * @author William Martin
+ * @version 0.1.0
+ *
+ * Each Simulation type should also implement a set of functions to be
+ * defined for and called by the LLM.
+ */
+
 export class Simulation {
   constructor () {
     
+  }
+  
+  get defaultContext () {
+    return `You are a helpful assistant..`
+  }
+  
+  get toolSchemas () {
+    // Should return functions chemas.
+    return [];
+  }
+  
+  get availableFunctions () {
+    // Should return a dictionary of function names to function implementations.
+    return {}
   }
   
   initialize () {
