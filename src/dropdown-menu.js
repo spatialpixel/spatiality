67d7322c413de5410e71260b7e7890b7ca3eb2d0
William Martin <william@spatialpixel.com>
2024-02-18 16:43:55 -0500

    Fix to dropdown menu template multi-fetch bug

--- src/dropdown-menu.js
+++ src/dropdown-menu.js
@@ -3,36 +3,22 @@ class DropdownMenu extends HTMLElement {
     constructor() {
         super();
 
-        // Fetch the template HTML file
-        fetch('dropdown-menu-template.html')
-            .then(response => response.text())
-            .then(html => {
-                // Create a template element and set its innerHTML to the fetched HTML
-                const template = document.createElement('template');
-                template.innerHTML = html;
+        const template = document.getElementById('dropdown-menu-template');
+        const templateContent = template.content;
+        
+        // Create shadow DOM and append template
+        this.attachShadow({ mode: 'open' });
 
-                // Select the template content
-                const templateContent = template.content.childNodes;
-
-                // Create shadow DOM and append template
-                this.attachShadow({ mode: 'open' });
-                
-                templateContent.forEach(node => {
-                    this.shadowRoot.appendChild(node.cloneNode(true));
-                });
-
-                // Add event listener to toggle dropdown
-                const toggleButton = this.shadowRoot.querySelector('.dropdown-toggle');
-                toggleButton.addEventListener('click', () => {
-                    this.closeAllDropdownsExceptThis();
-                    
-                    const dropdownContent = this.shadowRoot.querySelector('.dropdown-content');
-                    dropdownContent.classList.toggle('show');
-                });
-            })
-            .catch(error => {
-                console.error('Error fetching dropdown menu template:', error);
-            });
+        this.shadowRoot.appendChild(templateContent.cloneNode(true));
+        
+        // Add event listener to toggle dropdown
+        const toggleButton = this.shadowRoot.querySelector('.dropdown-toggle');
+        toggleButton.addEventListener('click', () => {
+            this.closeAllDropdownsExceptThis();
+            
+            const dropdownContent = this.shadowRoot.querySelector('.dropdown-content');
+            dropdownContent.classList.toggle('show');
+        });
     }
     
     closeAllDropdownsExceptThis () {
@@ -54,4 +40,5 @@ class DropdownMenu extends HTMLElement {
 }
 
 // Define the custom element
+console.debug('Defining dropdown-menu')
 customElements.define('dropdown-menu', DropdownMenu);
