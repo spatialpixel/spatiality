489e583fb633de13785765e8f0394381e610442a
William Martin <william@spatialpixel.com>
2024-02-18 22:22:54 -0500

    Tweaked some messaging in the openai chats

--- src/openai.js
+++ src/openai.js
@@ -210,7 +210,7 @@ export class OpenAIInterface {
       if (responseMessage.tool_calls) {
         const tool_names = responseMessage.tool_calls.map(t => t.function.name).join(', ');
 
-        this.addMessageToList('→ ' + (responseContent || `Need to call a function: ${tool_names}`));
+        this.addMessageToList('→ ' + (responseContent || `Response requires calling these functions: ${tool_names}`));
         
         // In this case, the response has asked to call one or more tools to get enough information
         // to complete the chat.
@@ -218,23 +218,23 @@ export class OpenAIInterface {
         for (const tool_call of responseMessage.tool_calls) {
           const function_name = tool_call.function.name;
   
-          this.addMessageToList(`··· Asking to call function: ${function_name}`);
+          this.addMessageToList(`··· Calling function: ${function_name}`);
           
           const function_to_call = this.state.availableFunctions[function_name];
           const function_args = JSON.parse(tool_call.function.arguments);
   
-          console.log('Asking to call function:', function_name, ', with the arguments:', function_args);
+          console.log('··· Calling function:', function_name, ', with the arguments:', function_args);
   
-          const function_response = function_to_call(function_args);
+          const function_result = function_to_call(function_args);
           
-          console.log('→ function response:', function_response);
+          console.log('··· Function result:', function_result);
           
           // Extend conversation with function's response.
           const functionResponseMessage = {
             "tool_call_id": tool_call.id,
             "role": "tool",
             "name": function_name,
-            "content": function_response,
+            "content": function_result,
           };
           chat.addMessage(functionResponseMessage);
         } // end loop of function calls
